04199b251add51bf6d862b5c3e60b6a8f0528fbf
Guangzhi Xie <guangzhi.xie@berkeley.edu>
2019-01-26 22:57:30 +0800

    Update location in DB in async

--- driver-service/src/main/java/com/driver/service/impl/UpdateLocationHandlerImpl.java
+++ driver-service/src/main/java/com/driver/service/impl/UpdateLocationHandlerImpl.java
@@ -7,9 +7,15 @@ import com.driver.web.model.LocationRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class UpdateLocationHandlerImpl implements UpdateLocationHandler {
+    //TODO: move to centralized config
+    private static final int CONCURRENCY_LEVEL = 5;
+
+    private static ExecutorService THREAD_POOL = Executors.newFixedThreadPool(CONCURRENCY_LEVEL);
 
     @Resource
     private DriverLocationCache driverLocationCache;
@@ -18,6 +24,12 @@ public class UpdateLocationHandlerImpl implements UpdateLocationHandler {
     public void handle(Integer id, LocationRequest locationRequest) {
         driverLocationCache.updateLocation(id, new LatLang(locationRequest.getLatitude(), locationRequest.getLongitude()));
         //TODO: handle accuracy
+
+        // update location in DB in async
+        THREAD_POOL.execute(()->updateLocationInDB(locationRequest));
+    }
+
+    private void updateLocationInDB(LocationRequest locationRequest) {
         //TODO: update location in DB
     }
 }
